7122fae9ae7aeac097d81a72d9095124a3ecd315
neeraj00890 <kumar74312@gmail.com>
2021-06-29 12:24:47 +0530

    fix: change variable name

--- realtime-pizza/routes/api.js
+++ realtime-pizza/routes/api.js
@@ -3,15 +3,14 @@ const router =express.Router();
 
 const cartController = require('../app/controller/customers/cartController')
 const homeController = require('../app/controller/homeController')
-const authController= require('../app/controller/authController')
+const authorizationController= require('../app/controller/authController')
 
 router.get('/fetchAll',homeController().getAllPizzas);
 router.post('/updateCart',cartController().updateCart);
 router.get('/getCart',cartController().getCart);
 
-
-router.post('/signUp',authController().signup);
-router.post('/login',authController().login);
+router.post('/signUp',authorizationController().signup);
+router.post('/login',authorizationController().login);
 
 
 
