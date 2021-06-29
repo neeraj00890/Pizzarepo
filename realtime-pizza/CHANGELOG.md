63d2d3b4fa35bb9581a2b48b49d1409ffde0c5fa
neeraj00890 <kumar74312@gmail.com>
2021-06-29 12:28:20 +0530

    BREAKING CHANGE: Drop support for node 6

--- realtime-pizza/CHANGELOG.md
+++ realtime-pizza/CHANGELOG.md
@@ -1,5 +1,15 @@
 #### 1.0.0 (2021-06-29)
 
+##### New Features
+
+*  integration  changelog (ea27fdbb)
+
+##### Bug Fixes
+
+*  change variable name (7122fae9)
+
+#### 1.0.0 (2021-06-29)
+
 ##### Bug Fixes
 
 *  change variable name (7122fae9)
--- realtime-pizza/routes/api.js
+++ realtime-pizza/routes/api.js
@@ -7,6 +7,10 @@ const authorizationController= require('../app/controller/authController')
 
 
 
+
+
+
+
 router.get('/fetchAll',homeController().getAllPizzas);
 router.post('/updateCart',cartController().updateCart);
 router.get('/getCart',cartController().getCart);
